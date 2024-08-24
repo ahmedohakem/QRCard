2f0bea8efb01fd3a07609e47c4af33789e2bb2bc
Ahmed Hakem <34775534+ahmedohakem@users.noreply.github.com>
2024-08-24 14:34:23 +0300

    Add files via upload

---
+++ includefiles/conn_db.php
@@ -0,0 +1,10 @@
+<?php
+//$conn = new mysqli("DB_HOST_NAME_OR_IP", "DB_USERNAME", "DB_PASSWORD","DB_NAME");
+/*
+// Check connection
+if ($conn -> connect_errno) {
+  //echo "Failed to connect to MySQL: " . $conn -> connect_error;
+  exit();
+}
+*/
+?>
